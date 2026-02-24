57a85db9687511eba9e09d953b45756aa036636f
Garvit Singla <garvits093@gmail.com>
2026-02-24 12:29:40 +0530

    Escrow contract done

--- escrow-program/programs/escrow-program/src/instructions/mod.rs
+++ escrow-program/programs/escrow-program/src/instructions/mod.rs
@@ -2,3 +2,5 @@ pub mod make;
 pub use make::*;
 pub mod refund;
 pub use refund::*;
+pub mod take;
+pub use take::*;
--- escrow-program/programs/escrow-program/src/lib.rs
+++ escrow-program/programs/escrow-program/src/lib.rs
@@ -13,14 +13,16 @@ pub mod escrow_program {
 
     pub fn make(ctx: Context<Make>, seed: u64, price: u64) -> Result<()> {
         ctx.accounts.init(seed, price, &ctx.bumps);
-
-
-        
         Ok(())
 
     }
     pub fn refund(ctx: Context<Refund>, seed: u64) -> Result<()> {
         ctx.accounts.refund(seed);
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>, seed: u64) -> Result<()> {
+        ctx.accounts.take(seed);
         Ok(())
     }
 }
